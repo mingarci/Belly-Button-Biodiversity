5bafbc47d99a840da95e05fa69c0d66bf43d1c32
Mindy Garcia <96347335+mingarci@users.noreply.github.com>
2022-04-02 17:59:38 -0500

    samples.json read

--- StarterCode/static/js/app.js
+++ StarterCode/static/js/app.js
@@ -1,3 +1,14 @@
 d3.csv("https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css", function(data) {
     console.log(data);
 });
+
+// Use the D3 library to read in `samples.json`.
+d3.json("samples.json").then((data => {
+    
+
+
+
+
+
+
+}
