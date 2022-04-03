f865295f748c9d0cb2d1f10c2d9b30fcabb6a1d7
Mindy Garcia <96347335+mingarci@users.noreply.github.com>
2022-04-02 20:27:53 -0500

    beginning of bar chart

--- StarterCode/static/js/app.js
+++ StarterCode/static/js/app.js
@@ -2,10 +2,26 @@ d3.csv("https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css", 
     console.log(data);
 });
 
-// Use the D3 library to read in `samples.json`.
+// * Use the D3 library to read in `samples.json`.
 d3.json("samples.json").then((data => {
-    
 
+// Create a horizontal bar chart with a dropdown menu to display the top 10 OTUs found in that individual.
+// * Use `sample_values` as the values for the bar chart.
+    const samples = data.samples
+    const metadata = data.metadata
+    const filtered = metadata.filter(bacteriaInfo => bacteriaInfo.id = patientID)[0]
+
+    const filteredSample = samples.filter(bacteriaInfo => bacteriaInfo.id = patientID)[0]
+
+ // * Use `sample_values` as the values for the bar chart.
+
+    const sample_values = filteredSample.sample_values
+
+// * Use `otu_ids` as the labels for the bar chart.
+    const otu_ids = filteredSample.otu_ids
+
+
+// * Use `otu_labels` as the hovertext for the chart.
 
 
 
